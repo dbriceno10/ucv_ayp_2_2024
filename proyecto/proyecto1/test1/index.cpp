85e2cfc7b6be2e111b851917ac0b438e65511924
Daniel Briceno <daniel.b100726@gmail.com>
2024-12-16 14:54:22 -0400

    test proyecto

---
+++ proyecto/proyecto1/test1/index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+
+  cout << " 0 " << " 0 " << " 0 " << " 0 " << " 0 " << " Meta " << endl;
+  cout << " J " << " 0 " << " Te " << " 0 " << " 0 " << " 0 " << endl;
+  cout << " 0 " << " 0 " << " 0 " << " M " << " 0 " << " 0 " << endl;
+  cout << " 0 " << " 0 " << " 0 " << " 0 " << " 0 " << " 0 " << endl;
+  cout << " 0 " << " 0 " << " 0 " << " 0 " << " M " << " 0 " << endl;
+  cout << " 0 " << " 0 " << " 0 " << " M " << " M " << " 0 " << endl;
+  cout << "  " << endl;
+  cout << "Caiste en una trampa, pierdes 10 de vida" << endl;
+  return 0;
+}
