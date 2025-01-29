d07ab2f7618be8731fa5c7e3b7a14c938c7888e0
Daniel Briceno <daniel.b100726@gmail.com>
2025-01-28 22:55:37 -0400

    practica 6, ejercicio 3

---
+++ practica-6/3/index.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  string palabra = "", capicua = "";
+  cout << "Ingrese una palabra" << endl;
+  cin >> palabra;
+  for (int i = palabra.length() - 1; i >= 0; i--)
+  {
+    capicua += palabra[i];
+  }
+
+  cout << palabra << " ----> " << capicua << endl;
+
+  if (palabra == capicua)
+  {
+    cout << "La palabra es capicua" << endl;
+  }
+  else
+  {
+    cout << "La palabra no es capicua" << endl;
+  }
+
+  return 0;
+}
